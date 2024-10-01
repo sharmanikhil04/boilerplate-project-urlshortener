2880fe4387f7830eed3c8492cf25fa2a8ca334a6
Nikhil Sharma <nikhil.sharma@tietoevry.com>
2024-10-01 12:47:55 +0530

    Update code to handle url shortener

--- index.js
+++ index.js
@@ -2,11 +2,29 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const bodyParser = require('body-parser');
+const dns = require('dns');
+app.use("/",bodyParser.urlencoded({extended: false}))
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.post('/api/shorturl',(req,res) => {
+  const original_url = req.body.url;
+  const urlObject = new URL(original_url);
+  dns.lookup(urlObject.hostname, (err, address, family) => {
+    if (err) {
+      res.json({
+        originalURL: originalURL,
+        shortenedURL: "Invalid URL"
+      });
+    } else {
+      var shortenedURL = Math.floor(Math.random() * 100000).toString();
+      res.json({"original_url": original_url, "short_url": shortenedURL})
+    }
+  })
+})
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
